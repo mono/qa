6da0830c574fd094cc34797879b427894eddf016
Rusty Howell <rhowell@mono-cvs.ximian.com>
2009-03-24 23:20:54 +0000

    * Added variables to monotesting * Added the README * Removed some test files that were not supposed to be added
    svn path=/trunk/qa/; revision=130174

--- selenium/monotesting.py
+++ selenium/monotesting.py
@@ -6,6 +6,7 @@
 # rc_browser='*chrome'
 
 base_url = 'http://mono-mac1.provo.novell.com'
+
 rc_server = 'localhost'
 rc_port = 4444
 rc_browser = '*chrome'
@@ -14,5 +15,8 @@ xsp1_port = 8081
 xsp2_port = 8082
 graffiti_port = 8083
 
-
 xsp1_url = '%s:%d' % (base_url,xsp1_port)
+xsp2_url = '%s:%d' % (base_url,xsp2_port)
+graffiti_url = '%s:%d' % (base_url,graffiti_port)
+
+
