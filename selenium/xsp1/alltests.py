2f38f355e45f79f67bd070935942dd4af54c2355
Rusty Howell <rhowell@mono-cvs.ximian.com>
2009-03-26 21:13:42 +0000

    * Fixed xsp1/alltests.py
    svn path=/trunk/qa/; revision=130338

--- selenium/xsp1/alltests.py
+++ selenium/xsp1/alltests.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python
-import unittest
 import sys
 
-sys.path.append('..')
-from monotesting import *
+
+
+sys.path.append('../..')
+from common.monotesting import *
 
 # sub directories
 # Add child test suites to test suite
+#from auth.alltests import *
+#from databinding.alltests import *
 from webservice.alltests import *
-from auth.alltests import *
-from databinding.alltests import *
-from handlers.alltests import *
-from customcontrols.alltests import *
-from htmlcontrols.alltests import *
-from webcontrols.alltests import *
-from asp_net_framework import *
+#from handlers.alltests import *
+#from customcontrols.alltests import *
+#from htmlcontrols.alltests import *
+#from webcontrols.alltests import *
+#from asp_net_framework import *
 
 
 if __name__ == '__main__':
