b1bd03b9ab0bfd620cd783d27ed3c3764327340b
Rusty Howell <rhowell@mono-cvs.ximian.com>
2009-03-26 20:43:12 +0000

    * Update webservice/alltests.py to work with new structure
    svn path=/trunk/qa/; revision=130336

--- selenium/xsp1/webservice/alltests.py
+++ selenium/xsp1/webservice/alltests.py
@@ -4,8 +4,8 @@ import sys,unittest
 from addTest import *
 from echoTest import *
 
-sys.path.append('../..')
-from monotesting import *
+sys.path.append('../../..')
+from common.monotesting import *
 
 if __name__ == '__main__':
     monotesting_main()
