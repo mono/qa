aa1f3ccaee17fb95f43b3705f994389ada9b71a3
Thomas Wiest <twiest@mono-cvs.ximian.com>
2009-03-27 22:34:00 +0000

    * Updated loginTest to the new style
    svn path=/trunk/qa/; revision=130440

--- selenium/xsp1/authentication/loginTest.py
+++ selenium/xsp1/authentication/loginTest.py
@@ -1,42 +1,50 @@
 #!/usr/bin/env python
 
+
 import sys
-sys.path.append('../..')
-from selenium import selenium
-from monotesting import *
+sys.path.append('../../..')
+from selenium.selenium import selenium
+from selenium.seleniumTestCase import *
+from common.monotesting import *
 
 import unittest, time, re
 
-class loginTest(unittest.TestCase):
-    def setUp(self):
-        self.testcaseid=426290
-        self.verificationErrors = []
-        self.selenium = selenium(rc_server, rc_port, rc_browser, xsp1_url)
-        self.selenium.start()
-    
-    def test_new(self):
-        sel = self.selenium
-        sel.open("/")
-        sel.click("link=login")
-        sel.wait_for_page_to_load("30000")
-        sel.type("UserEmail", "jdoe@somewhere.com")
-        sel.type("UserPass", "password")
-        sel.click("//html/body/form/input[2]")
-        sel.wait_for_page_to_load("30000")
-        sel.click("link=Authentication")
-        sel.wait_for_page_to_load("30000")
-        try:
-            self.assertEqual("Signout", sel.get_value("//html/body/form/input"))
-            passTestCase(self.testcaseid)
+class Authentication_LoginTest(seleniumTestCase):
+    testcaseid = 837598
 
-        except AssertionError, e:
-            self.verificationErrors.append(str(e))
-            failTestCase(self.testcaseid)
-    
-    def tearDown(self):
-        self.selenium.stop()
-        updateTestCase(self.testcaseid,len(self.verificationErrors) == 0)
-        self.assertEqual([], self.verificationErrors)
+    def test(self):
+        if not self.canRun:
+            return
+        try:
+            sel = self.selenium
+            sel.open("/")
+            sel.click("link=login")
+            sel.wait_for_page_to_load("30000")
+            sel.type("UserEmail", "jdoe@somewhere.com")
+            sel.type("UserPass", "password")
+            sel.click("_ctl4")
+            sel.wait_for_page_to_load("30000")
+            sel.click("link=Authentication")
+            sel.wait_for_page_to_load("30000")
+
+            try:
+                self.assertEqual("Signout", sel.get_value("//html/body/form/input[2]"))
+            except AssertionError, e:
+                self.verificationErrors.append(str(e))
+
+            sel.click("_ctl2")
+            sel.wait_for_page_to_load("30000")
+
+            try:
+                self.assertEqual("Login", sel.get_value("//html/body/form/input[2]"))
+            except AssertionError, e:
+                self.verificationErrors.append(str(e))
+        except AssertionError, e: self.verificationErrors.append(str(e))
+
 
 if __name__ == "__main__":
     monotesting_main()
+
+
+
+# vim:ts=4:expandtab:
