e6d7311bb484e70143fd2780cdc1954892bd5682
Thomas Wiest <twiest@mono-cvs.ximian.com>
2009-04-03 22:28:17 +0000

    * Converted arrayTest to the new way of doing test cases
    svn path=/trunk/qa/; revision=131028

--- selenium/xsp1/data_binding/arrayTest.py
+++ selenium/xsp1/data_binding/arrayTest.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.append('../..')
-from selenium import selenium
-from monotesting import *
+sys.path.append('../../..')
+from selenium.selenium import selenium
+from selenium.seleniumTestCase import *
+from common.monotesting import *
 
 import unittest, time, re
 
-class arrayTest(unittest.TestCase):
-    def setUp(self):
-        self.testcaseid = None
-        self.verificationErrors = []
-        self.selenium = selenium(rc_server, rc_port, rc_browser, xsp1_url)
-        self.selenium.start()
-    
+
+class DataBinding_arrayTest(seleniumTestCase):
+    testcaseid = 839038
+
     def test_new(self):
         sel = self.selenium
         sel.open("/")
@@ -31,11 +29,14 @@ class arrayTest(unittest.TestCase):
         sel.wait_for_page_to_load("30000")
         try: self.assertEqual("Selected option: One", sel.get_text("//*[@id=\"msg\"]"))
         except AssertionError, e: self.verificationErrors.append(str(e))
-    
-    def tearDown(self):
-        self.selenium.stop()
-        updateTestCase(self.testcaseid,len(self.verificationErrors) == 0)
-        self.assertEqual([], self.verificationErrors)
+
+
 
 if __name__ == "__main__":
     monotesting_main()
+
+
+
+
+
+# vim:ts=4:expandtab:
