8f017cd8ddfc603bd7ebb564c0f6e2c10653063a
Marc E. Christensen <mchristensen@mono-cvs.ximian.com>
2009-04-08 18:17:10 +0000

    Added new tests to framework
    svn path=/trunk/qa/; revision=131341

--- selenium/xsp1/web_controls/alltests.py
+++ selenium/xsp1/web_controls/alltests.py
@@ -4,23 +4,37 @@ sys.path.append('../../..')
 
 from common.monotesting import *
 
-
 from calendar import *
 from datalist import *
 from dbpage1 import *
 #from dbpage2 import *
 from listitem import *
+from temperature import *
+from validator1 import *
+from web_adrotator import *
+from web_button import *
 from web_checkboxlist import *
 from web_checkbox import *
 from web_comparevalidator import *
+from web_datagrid_command import *
 from web_datagrid import *
 from web_dropdownlist import *
+from web_hyperlink import *
 from web_imagebutton import *
+from web_image import *
+from web_label import *
+from web_linkbutton import *
 from web_listbox import *
 from web_literal import *
+from web_panel import *
+from web_placeholder import *
 from web_radiobuttonlist import *
+from web_radiobutton import *
 from web_regularexpressoinvalidator import *
+from web_repeater import *
+from web_table2 import *
 from web_table import *
+from web_textbox import *
 from web_xml import *
 
 
