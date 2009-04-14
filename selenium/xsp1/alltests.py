ec591ef59c5f74751256c640df7ed29fe449be1e
Marc E. Christensen <mchristensen@mono-cvs.ximian.com>
2009-04-14 23:54:56 +0000

    Added all 'alltests' from subdirectories so that running this alltests.py will actually run all the tests.
    svn path=/trunk/qa/; revision=131722

--- selenium/xsp1/alltests.py
+++ selenium/xsp1/alltests.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python
 import sys
 
-
-
 sys.path.append('../..')
 from common.monotesting import *
 
 # sub directories
 # Add child test suites to test suite
-#from auth.alltests import *
-#from databinding.alltests import *
-from web_service.alltests import *
-#from handlers.alltests import *
-#from customcontrols.alltests import *
-#from htmlcontrols.alltests import *
-#from webcontrols.alltests import *
-#from asp_net_framework import *
 
+from asp_net_framework.alltests import *
+from authentication.alltests import *
+from custom_controls.alltests import *
+from data_binding.alltests import *
+from handlers.alltests import *
+from html_controls.alltests import *
+from web_controls.alltests import *
+from web_service.alltests import *
 
 if __name__ == '__main__':
     monotesting_main()
-
-
 
 
 # vim:ts=4:expandtab:
