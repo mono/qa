109537956f128475ca552603ac2ef81c54c5902a
Thomas Wiest <twiest@mono-cvs.ximian.com>
2009-05-27 21:40:02 +0000

    * changed web_adrotator.py over to the new way of doing things * Fixed web_adrotator.py to check all variations of the rotated ad * Removed web_adrotator.html as there's no way to do such a complex task in html, so it doesn't line up at all with the python file
    svn path=/trunk/qa/; revision=134884

--- selenium/xsp1/web_controls/web_adrotator.py
+++ selenium/xsp1/web_controls/web_adrotator.py
@@ -1,29 +1,73 @@
 #!/usr/bin/env python
 
-
 import sys
 sys.path.append('../../..')
-from selenium.selenium import selenium
-from selenium.seleniumTestCase import *
-from common.monotesting import *
+import common.monotesting as mono
+from selenium.xsp1 import xsp1TestCase
 
 import unittest, time, re
 
-class WebControls_WebAdRotator(seleniumTestCase):
-    testcaseid = 839924
+class WebControls_WebAdRotator(xsp1TestCase.xsp1TestCase):
+    def __init__(self,methodname='test'):
+        xsp1TestCase.xsp1TestCase.__init__(self,methodname)
+        if not mono.usexsp2:
+            self.testcaseid = 839924 # xsp1 test case id
+        else:
+            self.testcaseid = 861798 # xsp2 test case id
 
-    def test(self):
-        sel = self.selenium
-        sel.open("/")
-        sel.click("link=web_adrotator")
+    def _runDancingMonkeyAd(self, sel):
+        while not sel.is_element_present("//img[@alt='Dancing monkey']"):
+            try:
+                self.failUnless(sel.is_element_present("//img[@alt='Mono']"))
+            except AssertionError, e:
+                self.verificationErrors.append(str(e))
+            sel.refresh()
+            sel.wait_for_page_to_load("30000")
+
+        sel.click("//html/body/form/a/img")
         sel.wait_for_page_to_load("30000")
-        #self.failUnless(sel.is_element_present("//html/body/form/a/img"))
+
+        # we don't know if the title for the novell.com/linux page will always be "Linux OS"
+        #self.assertEqual("Linux OS | SUSE Linux Enterprise", sel.get_title())
+
+        self.failUnless(sel.is_element_present("//html/body"))
+        sel.go_back()
+        sel.wait_for_page_to_load("30000")
+
+    def _runMonoAd(self, sel):
+        while not sel.is_element_present("//img[@alt='Mono']"):
+            try:
+                self.failUnless(sel.is_element_present("//img[@alt='Dancing monkey']"))
+            except AssertionError, e:
+                self.verificationErrors.append(str(e))
+            sel.refresh()
+            sel.wait_for_page_to_load("30000")
+
         sel.click("//html/body/form/a/img")
         sel.wait_for_page_to_load("30000")
         self.assertEqual("Main Page - Mono", sel.get_title())
+        sel.go_back()
+        sel.wait_for_page_to_load("30000")
+
+    def test(self):
+        if not self.canRun:
+            return
+        try:
+            sel = self.selenium
+            sel.open("/")
+            sel.click("link=web_adrotator")
+            sel.wait_for_page_to_load("30000")
+
+            for ix in range(5):
+                self._runDancingMonkeyAd(sel)
+                self._runMonoAd(sel)
+
+        except Exception,e:
+            self.verificationErrors.append(str(e))
+
 
 if __name__ == "__main__":
-    monotesting_main()
+    mono.monotesting_main()
 
 
 # vim:ts=4:expandtab:
