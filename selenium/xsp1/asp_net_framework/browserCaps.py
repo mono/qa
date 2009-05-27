91bfcb3b856e21d4bdb98af4e7977e6606ff625d
Thomas Wiest <twiest@mono-cvs.ximian.com>
2009-05-27 16:19:36 +0000

    * Fixed browserCaps.py to work under xsp1 and xsp2
    svn path=/trunk/qa/; revision=134842

--- selenium/xsp1/asp_net_framework/browserCaps.py
+++ selenium/xsp1/asp_net_framework/browserCaps.py
@@ -25,22 +25,40 @@ class AspNetFramework_browserCaps(xsp1TestCase.xsp1TestCase):
             sel.click("link=browsercaps")
             sel.wait_for_page_to_load("30000")
             try:
-                self.assertEqual("False", sel.get_text("//table[@id='dg']/tbody/tr[2]/td[2]"))
+                # The following xpath refers to the "ActiveXControls" "False" table entry
+                activeXControlsXPath = "//table[@id='dg']/tbody/tr[2]/td[2]"
+
+                self.assertEqual("False", sel.get_text(activeXControlsXPath))
             except AssertionError, e:
                 self.verificationErrors.append(str(e))
 
             try:
-                self.assertEqual("Firefox", sel.get_text("//table[@id='dg']/tbody/tr[6]/td[2]"))
+                # The following xpath refers to the "Browser" "Firefox" table entry
+                browserXPath = "//table[@id='dg']/tbody/tr[6]/td[2]"
+
+                self.assertEqual("Firefox", sel.get_text(browserXPath))
             except AssertionError, e:
                 self.verificationErrors.append(str(e))
 
             try:
-                self.assertEqual("True", sel.get_text("//table[@id='dg']/tbody/tr[19]/td[2]"))
+                # The following xpath refers to the "Tables" "True" table entry
+                if not mono.usexsp2:
+                    tablesXPath = "//table[@id='dg']/tbody/tr[19]/td[2]"
+                else:
+                    tablesXPath = "//table[@id='dg']/tbody/tr[20]/td[2]"
+
+                self.assertEqual("True", sel.get_text(tablesXPath))
             except AssertionError, e:
                 self.verificationErrors.append(str(e))
 
             try:
-                self.assertEqual("System.Web.UI.HtmlTextWriter", sel.get_text("//table[@id='dg']/tbody/tr[20]/td[2]"))
+                # The following xpath refers to the "TagWriter" "System.Web.UI.HtmlTextWriter" table entry
+                if not mono.usexsp2:
+                    tagWriterXPath = "//table[@id='dg']/tbody/tr[20]/td[2]"
+                else:
+                    tagWriterXPath = "//table[@id='dg']/tbody/tr[21]/td[2]"
+
+                self.assertEqual("System.Web.UI.HtmlTextWriter", sel.get_text(tagWriterXPath))
             except AssertionError, e:
                 self.verificationErrors.append(str(e))
 
