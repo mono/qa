4914ff0b07628911d006c535cb369d71a0ce4201
Thomas Wiest <twiest@mono-cvs.ximian.com>
2009-05-27 22:32:24 +0000

    * Changed web_controls/web_comparevalidator.py and web_controls/web_checkbox.py over to the new way of doing things
    svn path=/trunk/qa/; revision=134898

--- selenium/xsp1/web_controls/web_checkbox.py
+++ selenium/xsp1/web_controls/web_checkbox.py
@@ -1,33 +1,64 @@
 #!/usr/bin/env python
 
-
 import sys
 sys.path.append('../../..')
-from selenium.selenium import selenium
-from selenium.seleniumTestCase import *
-from common.monotesting import *
+import common.monotesting as mono
+from selenium.xsp1 import xsp1TestCase
 
 import unittest, time, re
 
-class WebControls_WebCheckBox(seleniumTestCase):
-    testcaseid = 838900
+class WebControls_WebCheckBox(xsp1TestCase.xsp1TestCase):
+    def __init__(self,methodname='test'):
+        xsp1TestCase.xsp1TestCase.__init__(self,methodname)
+        if not mono.usexsp2:
+            self.testcaseid = 838900 # xsp1 test case id
+        else:
+            self.testcaseid = 861804 # xsp2 test case id
+
+    def _checkCheckbox(self, sel, locator):
+        self.failUnless(sel.is_element_present(locator))
+        self.failIf(sel.is_checked(locator))
+        sel.check(locator)
+        self.failUnless(sel.is_checked(locator))
+
+    def _uncheckCheckbox(self, sel, locator):
+        self.failUnless(sel.is_element_present(locator))
+        self.failUnless(sel.is_checked(locator))
+        sel.uncheck(locator)
+        self.failIf(sel.is_checked(locator))
 
     def test(self):
-        sel = self.selenium
-        sel.open("/")
-        sel.click("link=web_checkbox")
-        sel.wait_for_page_to_load("30000")
-        self.failUnless(sel.is_element_present("//*[@id=\"chk\"]"))
-        self.failIf(sel.is_checked("//*[@id=\"chk\"]"))
-        sel.check("//*[@id=\"chk\"]")
-        self.failUnless(sel.is_checked("//*[@id=\"chk\"]"))
-        self.failUnless(sel.is_element_present("//*[@id=\"chk2\"]"))
-        self.failIf(sel.is_checked("//*[@id=\"chk2\"]"))
-        sel.check("//*[@id=\"chk2\"]")
-        self.failUnless(sel.is_checked("//*[@id=\"chk2\"]"))
-    
+        if not self.canRun:
+            return
+        try:
+            sel = self.selenium
+            sel.open("/")
+            sel.click("link=web_checkbox")
+            sel.wait_for_page_to_load("30000")
+
+            clickHereXPath = "//*[@id=\"chk\"]"
+            clickAlsoHereXPath = "//*[@id=\"chk2\"]"
+
+            # Check / uncheck all in order
+            self._checkCheckbox(sel, clickHereXPath)
+            self._checkCheckbox(sel, clickAlsoHereXPath)
+
+            self._uncheckCheckbox(sel, clickHereXPath)
+            self._uncheckCheckbox(sel, clickAlsoHereXPath)
+
+            # Check / uncheck one, then the other
+            self._checkCheckbox(sel, clickHereXPath)
+            self._uncheckCheckbox(sel, clickHereXPath)
+
+            self._checkCheckbox(sel, clickAlsoHereXPath)
+            self._uncheckCheckbox(sel, clickAlsoHereXPath)
+
+        except Exception,e:
+            self.verificationErrors.append(str(e))
+
+
 if __name__ == "__main__":
-    monotesting_main()
+    mono.monotesting_main()
 
 
 # vim:ts=4:expandtab:
--- selenium/xsp1/web_controls/web_comparevalidator.py
+++ selenium/xsp1/web_controls/web_comparevalidator.py
@@ -1,41 +1,58 @@
 #!/usr/bin/env python
 
-
 import sys
 sys.path.append('../../..')
-from selenium.selenium import selenium
-from selenium.seleniumTestCase import *
-from common.monotesting import *
+import common.monotesting as mono
+from selenium.xsp1 import xsp1TestCase
 
 import unittest, time, re
 
-class WebControls_WebCompareValidator(seleniumTestCase):
-    testcaseid = 838901
+class WebControls_WebCompareValidator(xsp1TestCase.xsp1TestCase):
+    def __init__(self,methodname='test'):
+        xsp1TestCase.xsp1TestCase.__init__(self,methodname)
+        if not mono.usexsp2:
+            self.testcaseid = 838901 # xsp1 test case id
+        else:
+            self.testcaseid = 861805 # xsp2 test case id
 
     def test(self):
-        sel = self.selenium
-        sel.open("/")
-        sel.click("link=web_comparevalidator")
-        sel.wait_for_page_to_load("30000")
-        self.failUnless(sel.is_text_present("Enter twice the same string:"))
-        sel.type("Text1", "asdfer adf we")
-        sel.type("Text2", "asdfer adf we")
-        sel.click("_ctl4")
-        sel.wait_for_page_to_load("30000")
-        self.failIf(sel.is_visible("//*[@id=\"_ctl3\"]"))
-        self.assertEqual("Entered data is valid.", sel.get_text("message"))
-        sel.type("Text2", "wrtvasd asdtrqwer")
-        sel.click("_ctl4")
-        self.failUnless(sel.is_visible("//*[@id=\"_ctl3\"]"))
-        sel.type("Text1", "Another test string")
-        sel.type("Text2", "Another test string")
-        sel.click("_ctl4")
-        sel.wait_for_page_to_load("30000")
-        self.failIf(sel.is_visible("//*[@id=\"_ctl3\"]"))
-        self.assertEqual("Entered data is valid.", sel.get_text("message"))
-    
+        if not self.canRun:
+            return
+        try:
+            sel = self.selenium
+            sel.open("/")
+            sel.click("link=web_comparevalidator")
+            sel.wait_for_page_to_load("30000")
+            self.failUnless(sel.is_text_present("Enter twice the same string:"))
+            sel.type("Text1", "asdfer adf we")
+            sel.type("Text2", "asdfer adf we")
+
+            if not mono.usexsp2:
+                checkButtonXPath = "_ctl4"
+                messageXPath = "//*[@id=\"_ctl3\"]"
+            else:
+                checkButtonXPath = "ctl04"
+                messageXPath = "//*[@id=\"ctl03\"]"
+
+            sel.click(checkButtonXPath)
+            sel.wait_for_page_to_load("30000")
+            self.failIf(sel.is_visible(messageXPath))
+            self.assertEqual("Entered data is valid.", sel.get_text("message"))
+            sel.type("Text2", "wrtvasd asdtrqwer")
+            sel.click(checkButtonXPath)
+            self.failUnless(sel.is_visible(messageXPath))
+            sel.type("Text1", "Another test string")
+            sel.type("Text2", "Another test string")
+            sel.click(checkButtonXPath)
+            sel.wait_for_page_to_load("30000")
+            self.failIf(sel.is_visible(messageXPath))
+            self.assertEqual("Entered data is valid.", sel.get_text("message"))
+        except Exception,e:
+            self.verificationErrors.append(str(e))
+
+
 if __name__ == "__main__":
-    monotesting_main()
+    mono.monotesting_main()
 
 
 # vim:ts=4:expandtab:
