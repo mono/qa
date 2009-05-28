7594f5a7111012139dc7ccbf23a1ee83567e2911
Marc E. Christensen <mchristensen@mono-cvs.ximian.com>
2009-05-28 20:18:55 +0000

    Change to cleaner style
    svn path=/trunk/qa/; revision=134987

--- selenium/xsp1/handlers/webhandlerTest.py
+++ selenium/xsp1/handlers/webhandlerTest.py
@@ -1,31 +1,24 @@
 #!/usr/bin/env python
 
-import sys
+import sys, unittest, time, re
+
 sys.path.append('../../..')
 import common.monotesting as mono
-from selenium.xsp1 import xsp1TestCase
+from selenium.xsp1.xsp1TestCase import xsp1TestCase
 
-import unittest, time, re
-
-class Handlers_WebHandler(xsp1TestCase.xsp1TestCase):
-    def __init__(self,methodname='test'):
-        xsp1TestCase.xsp1TestCase.__init__(self,methodname)
-        if not mono.usexsp2:
-            self.testcaseid = 840264
-        else:
-            self.testcaseid = 861702 
+class Handlers_WebHandler (xsp1TestCase):
+    xsp1TestCaseId = 840264
+    xsp2TestCaseId = 861702
 
     def test(self):
         if not self.canRun:
             return
         try: 
-
-            def test(self):
-                sel = self.selenium
-                sel.open("/")
-                sel.click("link=webhandler.ashx")
-                sel.wait_for_page_to_load("30000")
-                self.assertEqual("Hi there!", sel.get_text("//h1"))
+            sel = self.selenium
+            sel.open("/")
+            sel.click("link=webhandler.ashx")
+            sel.wait_for_page_to_load("30000")
+            self.assertEqual("Hi there!", sel.get_text("//h1"))
 
         except Exception,e:
             self.verificationErrors.append(str(e))
