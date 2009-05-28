4ad2cb57ba43b6c846be9bde6e77480ba662ea82
Thomas Wiest <twiest@mono-cvs.ximian.com>
2009-05-28 22:36:10 +0000

    * Changed web_datagrid.py over to the no-constructor test case way
    svn path=/trunk/qa/; revision=135015

--- selenium/xsp1/web_controls/web_datagrid.py
+++ selenium/xsp1/web_controls/web_datagrid.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python
 
+import sys, unittest, time, re
 
-import sys
 sys.path.append('../../..')
-from selenium.selenium import selenium
-from selenium.seleniumTestCase import *
-from common.monotesting import *
+import common.monotesting as mono
+from selenium.xsp1.xsp1TestCase import xsp1TestCase
 
-import unittest, time, re
 
-class WebControls_WebDataGrid(seleniumTestCase):
-    testcaseid = 838902
+class WebControls_WebDataGrid(xsp1TestCase):
+    xsp1TestCaseId = 838902
+    xsp2TestCaseId = 861963
+
+    def _verifyRow(self, rowXPath, country, continent, abbr):
+        self.assertEqual(country, self.selenium.get_text(rowXPath + "/td"))
+        self.assertEqual(continent, self.selenium.get_text(rowXPath + "/td[2]"))
+        self.assertEqual(abbr, self.selenium.get_text(rowXPath + "/td[3]"))
+        
 
     def test(self):
-        sel = self.selenium
-        sel.open("/")
-        sel.click("link=web_datagrid")
-        sel.wait_for_page_to_load("30000")
-        self.assertEqual("DataGrid sample", sel.get_text("//h3"))
-        self.assertEqual("Country", sel.get_text("//table[@id='dg']/tbody/tr[1]/td[1]"))
-        self.assertEqual("Continent", sel.get_text("//table[@id='dg']/tbody/tr[1]/td[2]"))
-        self.assertEqual("Abbr", sel.get_text("//table[@id='dg']/tbody/tr[1]/td[3]"))
-        self.assertEqual("Spain", sel.get_text("//table[@id='dg']/tbody/tr[2]/td[1]"))
-        self.assertEqual("Europe", sel.get_text("//table[@id='dg']/tbody/tr[2]/td[2]"))
-        self.assertEqual("es", sel.get_text("//table[@id='dg']/tbody/tr[2]/td[3]"))
-        self.assertEqual("Japan", sel.get_text("//table[@id='dg']/tbody/tr[3]/td[1]"))
-        self.assertEqual("Asia", sel.get_text("//table[@id='dg']/tbody/tr[3]/td[2]"))
-        self.assertEqual("jp", sel.get_text("//table[@id='dg']/tbody/tr[3]/td[3]"))
-        self.assertEqual("Mexico", sel.get_text("//table[@id='dg']/tbody/tr[4]/td[1]"))
-        self.assertEqual("America", sel.get_text("//table[@id='dg']/tbody/tr[4]/td[2]"))
-        self.assertEqual("mx", sel.get_text("//table[@id='dg']/tbody/tr[4]/td[3]"))
-    
+        try:
+            sel = self.selenium
+            sel.open("/")
+            sel.click("link=web_datagrid")
+            sel.wait_for_page_to_load("30000")
+            self.assertEqual("DataGrid sample", sel.get_text("//h3"))
+
+            headerRowXPath = "//table[@id='dg']/tbody/tr"
+            spainRowXPath = "//table[@id='dg']/tbody/tr[2]"
+            japanRowXPath = "//table[@id='dg']/tbody/tr[3]"
+            mexicoRowXPath = "//table[@id='dg']/tbody/tr[4]"
+
+            self._verifyRow(headerRowXPath, "Country", "Continent", "Abbr")
+            self._verifyRow(spainRowXPath, "Spain", "Europe", "es")
+            self._verifyRow(japanRowXPath, "Japan", "Asia", "jp")
+            self._verifyRow(mexicoRowXPath, "Mexico", "America", "mx")
+
+        except Exception,e:
+            self.verificationErrors.append(str(e))
+
+
 if __name__ == "__main__":
-    monotesting_main()
+    mono.monotesting_main()
 
 
 # vim:ts=4:expandtab:
