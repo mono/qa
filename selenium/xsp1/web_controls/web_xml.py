ea0f40c9b28b6c2b308634721794d147dffa66e2
Marc E. Christensen <mchristensen@mono-cvs.ximian.com>
2009-05-28 21:17:12 +0000

    Fixed isses when running against xsp2
    svn path=/trunk/qa/; revision=135001

--- selenium/xsp1/web_controls/web_xml.py
+++ selenium/xsp1/web_controls/web_xml.py
@@ -14,17 +14,21 @@ class WebControls_WebXML(xsp1TestCase):
         if not self.canRun:
             return
         try:
+            if not mono.usexsp2:
+                FormCtl = "_ctl1"
+            else:
+                FormCtl = "ctl01"
             sel = self.selenium
             sel.open("/")
             sel.click("link=web_xml")
             sel.wait_for_page_to_load("30000")
             self.assertEqual("Xml Example", sel.get_text("//h3"))
-            self.assertEqual("Joe   Suits", sel.get_text("//form[@id='_ctl1']/table[1]/tbody/tr[1]/td/b"))
-            self.assertEqual("Job Title: CEO\n Description: Wears the nice suit", sel.get_text("//form[@id='_ctl1']/table[1]/tbody/tr[3]/td"))
-            self.assertEqual("Jeremy   Boards", sel.get_text("//form[@id='_ctl1']/table[3]/tbody/tr[1]/td/b"))
-            self.assertEqual("Job Title: Pro Surfer\n Description: Rides the big waves", sel.get_text("//form[@id='_ctl1']/table[3]/tbody/tr[3]/td"))
-            self.assertEqual("Joan   Page", sel.get_text("//form[@id='_ctl1']/table[4]/tbody/tr[1]/td/b"))
-            self.assertEqual("Job Title: Web Site Developer\n Description: Writes the pretty pages", sel.get_text("//form[@id='_ctl1']/table[4]/tbody/tr[3]/td"))
+            self.assertEqual("Joe   Suits", sel.get_text("//form[@id='" + FormCtl + "']/table[1]/tbody/tr[1]/td/b"))
+            self.assertEqual("Job Title: CEO\n Description: Wears the nice suit", sel.get_text("//form[@id='" + FormCtl + "']/table[1]/tbody/tr[3]/td"))
+            self.assertEqual("Jeremy   Boards", sel.get_text("//form[@id='" + FormCtl + "']/table[3]/tbody/tr[1]/td/b"))
+            self.assertEqual("Job Title: Pro Surfer\n Description: Rides the big waves", sel.get_text("//form[@id='" + FormCtl + "']/table[3]/tbody/tr[3]/td"))
+            self.assertEqual("Joan   Page", sel.get_text("//form[@id='" + FormCtl + "']/table[4]/tbody/tr[1]/td/b"))
+            self.assertEqual("Job Title: Web Site Developer\n Description: Writes the pretty pages", sel.get_text("//form[@id='" + FormCtl + "']/table[4]/tbody/tr[3]/td"))
   
         except Exception,e:
             self.verificationErrors.append(str(e))
