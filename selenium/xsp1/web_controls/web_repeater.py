25c0ee174819c4ad0d32dd6b41df3bf73edbc28e
Marc E. Christensen <mchristensen@mono-cvs.ximian.com>
2009-05-28 21:11:04 +0000

    Fixed problems running web_repeater against xsp2
    svn path=/trunk/qa/; revision=134999

--- selenium/xsp1/web_controls/web_repeater.py
+++ selenium/xsp1/web_controls/web_repeater.py
@@ -14,23 +14,27 @@ class WebControls_WebRepeater(xsp1TestCase):
         if not self.canRun:
             return
         try:
+            if not mono.usexsp2:
+                FormCtl = "_ctl1"
+            else:
+                FormCtl = "ctl01"
             sel = self.selenium
             sel.open("/")
             sel.click("link=web_repeater")
             sel.wait_for_page_to_load("30000")
             self.failUnless(sel.is_element_present("//html/body/form/table"))
-            self.assertEqual("Country", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[1]/td[1]/b"))
-            self.assertEqual("Abbreviation", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[1]/td[2]/b"))
-            self.assertEqual("Continent", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[1]/td[3]/b"))
-            self.assertEqual("Spain", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[2]/td[1]"))
-            self.assertEqual("es", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[2]/td[2]"))
-            self.assertEqual("Europe", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[2]/td[3]"))
-            self.assertEqual("Japan", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[3]/td[1]"))
-            self.assertEqual("jp", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[3]/td[2]"))
-            self.assertEqual("Asia", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[3]/td[3]"))
-            self.assertEqual("Mexico", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[4]/td[1]"))
-            self.assertEqual("mx", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[4]/td[2]"))
-            self.assertEqual("America", sel.get_text("//form[@id='_ctl1']/table/tbody/tr[4]/td[3]"))
+            self.assertEqual("Country", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[1]/td[1]/b"))
+            self.assertEqual("Abbreviation", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[1]/td[2]/b"))
+            self.assertEqual("Continent", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[1]/td[3]/b"))
+            self.assertEqual("Spain", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[2]/td[1]"))
+            self.assertEqual("es", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[2]/td[2]"))
+            self.assertEqual("Europe", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[2]/td[3]"))
+            self.assertEqual("Japan", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[3]/td[1]"))
+            self.assertEqual("jp", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[3]/td[2]"))
+            self.assertEqual("Asia", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[3]/td[3]"))
+            self.assertEqual("Mexico", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[4]/td[1]"))
+            self.assertEqual("mx", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[4]/td[2]"))
+            self.assertEqual("America", sel.get_text("//form[@id='" + FormCtl + "']/table/tbody/tr[4]/td[3]"))
 
         except Exception,e:
             self.verificationErrors.append(str(e))
