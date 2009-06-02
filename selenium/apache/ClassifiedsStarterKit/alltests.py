c834cb304875876b5c08eec19926505f60bfaeeb
Marc E. Christensen <mchristensen@mono-cvs.ximian.com>
2009-06-02 23:07:39 +0000

    New directory structure for tests which run under mod_mono in Apache.
    svn path=/trunk/qa/; revision=135256

---
+++ selenium/apache/ClassifiedsStarterKit/alltests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import unittest,sys
+sys.path.append('../../..')
+
+from common.monotesting import *
+
+#from testFile import *
+
+if __name__ == '__main__':
+    monotesting_main()
---
+++ selenium/apache/alltests.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import sys
+
+sys.path.append('../..')
+from common.monotesting import *
+
+# sub directories
+# Add child test suites to test suite
+
+#from blogengine.alltests import *
+#from mojoportal.alltests import *
+#from BlogStarterKit.alltests import *
+#from ClassifiedsStarterKit.alltests import *
+#from ClubWebSite.alltests import *
+#from MonoForums.alltests import *
+
+if __name__ == '__main__':
+    monotesting_main()
+
+# vim:ts=4:expandtab:
