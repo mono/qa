1ebd4321dfc7fbb0741f13bb5da16e7c9ba5cefd
Thomas Wiest <twiest@mono-cvs.ximian.com>
2009-06-02 22:15:06 +0000

    * Created a test case for menu2.py
    svn path=/trunk/qa/; revision=135248

---
+++ selenium/xsp2/menu/menu2.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import sys, unittest, time, re
+
+sys.path.append('../../..')
+import common.monotesting as mono
+from selenium.xsp2.xsp2TestCase import xsp2TestCase
+
+
+class menu_menu2(xsp2TestCase):
+    testcaseid = 863393
+
+    def _isMenuRed(self, jsLocator):
+        className = self.selenium.get_eval("this.browserbot.findElement(" + jsLocator + ").className")
+        return (className == "Menu12_1 Menu12_3 Menu12_11")
+
+    def _isMenuItemRed(self, jsLocator):
+        className = self.selenium.get_eval("this.browserbot.findElement(" + jsLocator + ").className")
+        return (className == "Menu12_1 Menu12_6 Menu12_13")
+
+    def _isMenuItemYellow(self, jsLocator):
+        className = self.selenium.get_eval("this.browserbot.findElement(" + jsLocator + ").className")
+        return (className == "Menu12_1 Menu12_6 Menu12_9")
+
+    def _hoverMenuTest(self, jsLocator):
+        sel = self.selenium
+        locatorXPath = "//*[@" + jsLocator + "]"
+        sel.mouse_over(locatorXPath)
+        self.failUnless(self._isMenuRed(jsLocator))
+        sel.mouse_out(locatorXPath)
+        self.failIf(self._isMenuRed(jsLocator))
+
+    def _hoverSelectMenuItemTest(self, jsLocator):
+        sel = self.selenium
+        locatorXPath = "//*[@" + jsLocator + "]"
+        sel.mouse_over(locatorXPath)
+        self.failUnless(self._isMenuItemRed(jsLocator))
+        sel.click(locatorXPath)
+        sel.wait_for_page_to_load("30000")
+        self.failUnless(self._isMenuItemYellow(jsLocator))
+
+    def test(self):
+        if not self.canRun:
+            return
+        try:
+            # These are the main menus
+            menus = { "file":"id=\"Menu12_0l\"",
+                      "edit":"id=\"Menu12_1l\"" }
+
+            # These are the sub-menu items
+            menuItems = { "fileNew":"id=\"Menu12_0_0l\"",
+                          "fileOpen":"id=\"Menu12_0_1l\"",
+                          "fileClose":"id=\"Menu12_0_2l\"",
+                          "editCut":"id=\"Menu12_1_0l\"",
+                          "editCopy":"id=\"Menu12_1_1l\"",
+                          "editPaste":"id=\"Menu12_1_2l\"",
+                          "editEdit":"id=\"Menu12_1_3l\"",
+                          "editEditCut":"id=\"Menu12_1_3_0l\"",
+                          "editEditCopy":"id=\"Menu12_1_3_1l\"",
+                          "editEditPaste":"id=\"Menu12_1_3_2l\"",
+                          "editEditSelectAll":"id=\"Menu12_1_3_3l\"",
+                          "editSelectAll":"id=\"Menu12_1_4l\"",
+                          "editALargeOne":"id=\"Menu12_1_5l\"" }
+
+            # Add the Edit/A large one/Option sub-menu items
+            for ix in range(0, 60):
+                menuItems["option" + str(ix+1)] = "id=\"Menu12_1_5_" + str(ix) + "l\""
+
+            # Finally start testing
+            sel = self.selenium
+            sel.open("/2.0/menu/menu2.aspx")
+            sel.wait_for_page_to_load("30000")
+
+            # Test the upper menus (file and edit)
+            for menu in menus:
+                self._hoverMenuTest(menus[menu])
+
+            # Test the sub-menu items (new, cut, etc)
+            for menuItem in menuItems:
+                self._hoverSelectMenuItemTest(menuItems[menuItem])
+
+        except Exception,e:
+            self.verificationErrors.append(str(e))
+
+
+if __name__ == "__main__":
+    mono.monotesting_main()
+
+
+# vim:ts=4:expandtab:
