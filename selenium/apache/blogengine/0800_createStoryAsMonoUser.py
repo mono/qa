2b247ec4fba1b651b15003ed169db27c9488871d
Marc E. Christensen <mchristensen@mono-cvs.ximian.com>
2009-07-14 22:01:39 +0000

    New tests and some updates
    svn path=/trunk/qa/; revision=137902

--- selenium/apache/blogengine/0800_createStoryAsMonoUser.py
+++ selenium/apache/blogengine/0800_createStoryAsMonoUser.py
@@ -28,6 +28,11 @@ class aaCreateCategory(apacheTestCase):
                 sel.wait_for_page_to_load("30000")
             sel.type("ctl00_cphAdmin_txtRawContent", "<p>This is a story by mono user<p>")
             sel.type("ctl00_cphAdmin_txtRawContent", "<p>This is a story by mono user<p>\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer molestie justo a augue facilisis interdum. Sed sollicitudin lobortis dapibus. Suspendisse vitae urna urna, et posuere turpis! Nulla sed erat dui, eget interdum odio. Nullam id tortor et elit volutpat vulputate ac sit amet odio. Proin nisl est, pretium non dapibus id, vestibulum sed orci. Mauris lobortis diam eu mi pulvinar ut vehicula velit convallis. Quisque malesuada porttitor ante ut porta! Nunc magna ipsum, lobortis vitae luctus eu, varius et quam! Sed non metus imperdiet massa aliquam tristique sit amet auctor lacus. Maecenas sollicitudin varius aliquet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin non ipsum metus, vel elementum quam! Sed interdum metus pharetra elit accumsan sit amet semper purus auctor. Nunc eget dolor ac nibh molestie iaculis in at mi. Nunc velit nisl, venenatis at tempus id, adipiscing et ligula. Aliquam erat volutpat. Mauris eget commodo erat.</p>")
+            cat_check_xpath = "//span[@id='ctl00_cphAdmin_cblCategories']/label[text()='Mono']/preceding-sibling::input[1]"
+            if sel.is_element_present(cat_check_xpath) and not sel.is_checked(cat_check_xpath):
+                sel.click(cat_check_xpath)
+            else:
+                mono.log("No category 'Mono' exists.  Not setting a catagory.")
             sel.click("ctl00_cphAdmin_btnSave")
             sel.wait_for_page_to_load("30000")
             sel.click("link=Home")
