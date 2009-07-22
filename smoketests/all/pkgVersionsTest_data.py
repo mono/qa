76d5ab9f3c0e0851399082668edb076034b41a88
Andrew Jorgensen <ajorgensen@novell.com>
2009-07-22 18:09:57 +0000

    fix versions
    svn path=/trunk/qa/; revision=138432

--- smoketests/all/pkgVersionsTest_data.py
+++ smoketests/all/pkgVersionsTest_data.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 
 pkgs = {
-    'pkg-config --modversion atk':  '1.22.0',
-    'pkg-config --modversion gnome-sharp-2.0':  '2.20.1',
-    'pkg-config --modversion gtk-sharp-2.0':    '2.12.8',
-    'pkg-config --modversion mono':             '2.5',
-    'pkg-config --modversion mono-addins':      '0.5',
+    'pkg-config --modversion atk':  '1.24.0',
+    'pkg-config --modversion gnome-sharp-2.0':  '2.24.0',
+    'pkg-config --modversion gtk-sharp-2.0':    '2.12.9',
+    'pkg-config --modversion mono':             '2.4.2.3',
+    'pkg-config --modversion mono-addins':      '0.4',
     'pkg-config --modversion mono-addins-gui' : '0.4',
     'pkg-config --modversion mono-addins-setup':'0.4',
-    'pkg-config --modversion mono-cairo':       '2.2',
+    'pkg-config --modversion mono-cairo':       '2.4.2.3',
     'pkg-config --modversion mono-lineeditor':  '0.2.1',
-    'pkg-config --modversion mono-nunit':       '2.2',
+    'pkg-config --modversion mono-nunit':       '2.4.2.3',
     'pkg-config --modversion mono-options':     '0.2.1',
     'pkg-config --modversion mono-zeroconf':    '0.8.0',
-    'pkg-config --modversion monodevelop':      '1.9.1',
-    'pkg-config --modversion monodevelop-core-addins':'1.9.1',
-    'pkg-config --modversion monodoc':          '2.2',
-    'pkg-config --modversion smcs':             '2.5',
-    'pkg-config --modversion xsp':              '2.4',
+    'pkg-config --modversion monodevelop':      '2.0',
+    'pkg-config --modversion monodevelop-core-addins':'2.0',
+    'pkg-config --modversion monodoc':          '2.4.2.3',
+    'pkg-config --modversion smcs':             '2.4.2.3',
+    'pkg-config --modversion xsp':              '2.4.2',
 
 
 }
