15fa2e934e3ef7cf855e0d6656f53a2b1a0eb595
Rusty Howell <rhowell@mono-cvs.ximian.com>
2009-07-22 22:23:59 +0000

    * Updated versions for pkgconfig test
    svn path=/trunk/qa/; revision=138465

--- smoketests/all/pkgVersionsTest_data.py
+++ smoketests/all/pkgVersionsTest_data.py
@@ -2,13 +2,14 @@
 
 # These packages are common across all platforms
 pkgs = {
-    'pkg-config --modversion atk':  '1.24.0',
+    #'pkg-config --modversion atk':  '1.24.0',
     'pkg-config --modversion gtk-sharp-2.0':    '2.12.9',
     'pkg-config --modversion mono':             '2.4.2.3',
     'pkg-config --modversion mono-cairo':       '2.4.2.3',
     'pkg-config --modversion mono-lineeditor':  '0.2.1',
     'pkg-config --modversion mono-nunit':       '2.4.2.3',
     'pkg-config --modversion mono-options':     '0.2.1',
+    'pkg-config --modversion mono.web':         '2.0.0.0',
     'pkg-config --modversion monodoc':          '2.4.2.3',
     'pkg-config --modversion smcs':             '2.4.2.3',
     'pkg-config --modversion xsp':              '2.4.2',
@@ -17,24 +18,21 @@ pkgs = {
 # These packages are unique to linux
 linux_pkgs = {
     'pkg-config --modversion gnome-sharp-2.0':  '2.24.0',
-    'pkg-config --modversion mono-addins-gui' : '0.4',
-    'pkg-config --modversion mono-zeroconf':    '0.8.0',
-    'pkg-config --modversion monodevelop-core-addins':'2.0',
-    'pkg-config --modversion mono-addins-setup':'0.4',
     'pkg-config --modversion mono-addins':      '0.4',
+    'pkg-config --modversion mono-addins-gui' : '0.4',
+    'pkg-config --modversion mono-addins-setup':'0.4',
+    'pkg-config --modversion mono-zeroconf':    '0.8.0',
     'pkg-config --modversion monodevelop':      '2.0',
+    'pkg-config --modversion monodevelop-core-addins':'2.0',
 }
 
 
 # These packages are unique to OSX
 macos_pkgs = { 
-    'pkg-config --modversion cocoa-sharp':              '0.9.5',
-    'pkg-config --modversion mono-addins-gui' : '0.4',
-    'pkg-config --modversion mono-zeroconf':    '0.8.0',
-    'pkg-config --modversion monodevelop-core-addins':'2.0',
-    'pkg-config --modversion mono-addins-setup':'0.4',
+    'pkg-config --modversion cocoa-sharp':      '0.9.5',
     'pkg-config --modversion mono-addins':      '0.4',
-    'pkg-config --modversion monodevelop':      '2.0',
+    'pkg-config --modversion mono-addins-gui' : '0.4',
+    'pkg-config --modversion mono-addins-setup':'0.4',
 
 }
 
