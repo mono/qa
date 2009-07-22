469a3064542bb164191766bc882226c4387d254d
Marc E. Christensen <mchristensen@mono-cvs.ximian.com>
2009-07-22 19:02:31 +0000

    Looks like we still have to wait a second for the elements to appear even though selenium reports that they are.
    svn path=/trunk/qa/; revision=138446

--- selenium/apache/ClubWebSite/bbCreateEvent.py
+++ selenium/apache/ClubWebSite/bbCreateEvent.py
@@ -24,13 +24,29 @@ class ClugWebSite_bb_CreateEvent(apacheTestCase):
             sel.wait_for_page_to_load("30000")
             sel.type("ctl00_ContentPlaceHolder1_FormView1_titleTextBox", "Mono Summit in Down Under")
             sel.select("ctl00_ContentPlaceHolder1_FormView1_LocationPicker1_locationselect", "label=New Zealand")
+
             for i in range(60):
                 try:
-                    if sel.is_element_present("//option[@value='1']"): break
+                    if sel.is_element_present("//option[text()='New Zealand']"):
+                        mono.log("Found //option[text()='New Zealand']")
+                        break
                 except: pass
+                mono.log("Looking for //option[text()='New Zealand']")
                 time.sleep(1)
-            sel.click("//option[@value='1']")
+            time.sleep(1)
+            sel.click("//option[text()='New Zealand']")
+
+            for i in range(60):
+                try:
+                    if sel.is_element_present("ctl00_ContentPlaceHolder1_FormView1_descriptionTextBox"):
+                        mono.log("Found: ctl00_ContentPlaceHolder1_FormView1_descriptionTextBox")
+                        break
+                except: pass
+                mono.log("Looking for: ctl00_ContentPlaceHolder1_FormView1_descriptionTextBox")
+                time.sleep(1)
+            time.sleep(1)
             sel.type("ctl00_ContentPlaceHolder1_FormView1_descriptionTextBox", "The conference will be held this year in New Zealand, land where J R R Tolkien's novels of heroism, bravery, ultimate quests and triumph of good over evil were portrayed on the silver screen. Much hacking will be done.")
+
             sel.click("//*[@id=\"ctl00_ContentPlaceHolder1_FormView1_apply1\"]")
             sel.wait_for_page_to_load("30000")
             sel.click("ctl00_TopNavRepeat_ctl01_HyperLink1")
