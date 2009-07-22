28875e5126e8aeb239150206d7a0ab009be2f723
Marc E. Christensen <mchristensen@mono-cvs.ximian.com>
2009-07-22 18:06:14 +0000

    Comment out blogengine.net for now.  Testcase IDs need to be added
    svn path=/trunk/qa/; revision=138430

--- selenium/apache/alltests.py
+++ selenium/apache/alltests.py
@@ -7,6 +7,7 @@ from common.monotesting import *
 # sub directories
 # Add child test suites to test suite
 
+
 #from blogengine.alltests import *
 #from mojoportal.alltests import *
 from BlogStarterKit.alltests import *
