b648b7a65d9df0e4875ce320e98ab7740d94b73f
Thomas Wiest <twiest@mono-cvs.ximian.com>
2009-08-18 17:41:55 +0000

    * Fixed the MonoVS verifyZypperReposCanRefresh so that it works without the creds file there (it created then deletes the creds file so this test case still works)
    svn path=/trunk/qa/; revision=140178

--- smoketests/appliances/all/verifyZypperReposCanRefresh.py
+++ smoketests/appliances/all/verifyZypperReposCanRefresh.py
@@ -10,13 +10,27 @@ if not basepath in sys.path:
 
 import common.monotesting as mono
 from smoketests.smokeTestCase import smokeTestCase
+from common.helpers import *
 
 
 class verifyReposCanRefresh(smokeTestCase):
     testcaseid = 546327
 
     def test(self):
+        monovs_creds_file = "/etc/zypp/credentials.d/MonoVS"
+
+        if isMonoVSAppliance():
+            self.assertFalse(os.path.isfile("/etc/zypp/credentials.d/MonoVS"), "The MonoVS Repo Creds File exists!")
+            fd = open(monovs_creds_file, "wb")
+            fd.write("username=mono-vsbeta\n")
+            fd.write("password=VfSpW8lIPQ\n")
+            fd.close();
+
         self.canZypperReposBeRefreshed()
+
+        if isMonoVSAppliance():
+            # Clean up
+            os.remove(monovs_creds_file)
 
 if __name__ == "__main__":
     mono.monotesting_main()
