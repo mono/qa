2580b2a1745a7c1f76d31df9c878876c694600b3
Marc Christensen <mecworks@gmail.com>
2010-08-26 17:08:46 -0600

    Fixed failing browserCaps test

--- selenium/xsp1/asp_net_framework/browserCaps.py
+++ selenium/xsp1/asp_net_framework/browserCaps.py
@@ -24,37 +24,45 @@ class AspNetFramework_browserCaps(xsp1TestCase):
 
                 self.assertEqual("False", sel.get_text(activeXControlsXPath))
             except AssertionError, e:
-                self.verificationErrors.append(str(e))
+                self.verificationErrors.append("ActiveXControls test: " + str(e))
 
             try:
                 # The following xpath refers to the "Browser" "Firefox" table entry
-                browserXPath = "//table[@id='dg']/tbody/tr[6]/td[2]"
-
-                self.assertEqual("Firefox", sel.get_text(browserXPath))
+                if not mono.usexsp2:
+                    browserXPath = "//table[@id='dg']/tbody/tr[6]/td[2]"
+                else:
+                    browserXPath = "//table[@id='dg']/tbody/tr[7]/td[2]"
+                browser_txt = sel.get_text(browserXPath)
+                if browser_txt == "Firefox" or browser_txt == "Mozilla":
+                    txt_ok = "True"
+                else:
+                    txt_ok = "False"
+                print(browser_txt + browserXPath)
+                self.assertEqual(txt_ok, "True")
             except AssertionError, e:
-                self.verificationErrors.append(str(e))
+                self.verificationErrors.append("Browser test: " + str(e))
 
             try:
                 # The following xpath refers to the "Tables" "True" table entry
                 if not mono.usexsp2:
-                    tablesXPath = "//table[@id='dg']/tbody/tr[19]/td[2]"
-                else:
                     tablesXPath = "//table[@id='dg']/tbody/tr[20]/td[2]"
+                else:
+                    tablesXPath = "//table[@id='dg']/tbody/tr[107]/td[2]"
 
                 self.assertEqual("True", sel.get_text(tablesXPath))
             except AssertionError, e:
-                self.verificationErrors.append(str(e))
+                self.verificationErrors.append("Tables test: " + str(e))
 
             try:
                 # The following xpath refers to the "TagWriter" "System.Web.UI.HtmlTextWriter" table entry
                 if not mono.usexsp2:
-                    tagWriterXPath = "//table[@id='dg']/tbody/tr[20]/td[2]"
-                else:
                     tagWriterXPath = "//table[@id='dg']/tbody/tr[21]/td[2]"
+                else:
+                    tagWriterXPath = "//table[@id='dg']/tbody/tr[108]/td[2]"
 
                 self.assertEqual("System.Web.UI.HtmlTextWriter", sel.get_text(tagWriterXPath))
             except AssertionError, e:
-                self.verificationErrors.append(str(e))
+                self.verificationErrors.append("System.Web.UI.HtmlTextWriter test: " + str(e))
 
         except Exception,e:
             self.verificationErrors.append(str(e))
