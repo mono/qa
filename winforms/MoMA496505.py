122d1e8eb6d13b0683121f66a764fd004b83f966
David Mulder <dmulder@novell.com>
2011-04-15 09:43:24 -0700

    Modified MoMA test case to remove definitions as root (otherwise this test fails).

--- winforms/MoMA496505.py
+++ winforms/MoMA496505.py
@@ -9,8 +9,8 @@ from winformsTestCase import winformsTestCase
 
 class WinForms_MoMA496505Test(winformsTestCase):
     testcaseid = 496505
-    command = 'mv -v /usr/lib/moma/Definitions/* /tmp/;moma'
-    message = 'Click Next\nClick on Check for newer version'
+    command = 'sudo mv -v /usr/lib/moma/Definitions/* /tmp/;moma'
+    message = 'Enter the root password in the terminal\nClick Next\nClick on Check for newer version'
 
 
 if __name__ == "__main__":
