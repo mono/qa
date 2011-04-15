f2ac2397e164525ae7b6be5186b8559b618159a9
David Mulder <dmulder@novell.com>
2011-04-15 16:39:38 -0600

    Changed directory structure.

--- winforms/alltests.py
+++ winforms/alltests.py
@@ -1,66 +1,14 @@
 #!/usr/bin/python
 import sys
 
-sys.path.append('..')
+sys.path.append('../..')
 from common.monotesting import *
 
 # sub directories
 # Add child test suites to test suite
 
-from AderPlotter426249 import *
-from AderPlotter426251 import *
-from AnotherTetrisClone426253 import *
-from ChessBoard426255 import *
-from ChessBoard426256 import *
-from ChessBoard426257 import *
-from ControlInspector426259 import *
-from CSharpTetris426254 import *
-from GATetris433810 import *
-from GATetris426260 import *
-from GBrainy738631 import *
-from GraphLibraryDemo426261 import *
-from ICanSpy426262 import *
-from IEClone709763 import *
-from MoMA496504 import *
-from MoMA496505 import *
-from MonoCalendar426263 import *
-from MonoCalendar426266 import *
-from MonoCalendar426265 import *
-from MonoCalendar426264 import *
-from Monodoc429866 import *
-from Monodoc429867 import *
-from Monodoc429869 import *
-from Monodoc429868 import *
-from Monodoc429870 import *
-from myUML426288 import *
-from Notepad426271 import *
-from Notepad426269 import *
-from Notepad426270 import *
-from NPlot426268 import *
-from NPlot426267 import *
-from PieChart426272 import *
-from PieChart860690 import *
-from PieChart860689 import *
-from PieChart860688 import *
-from PolygonTriangulation426275 import *
-from PolygonTriangulation426276 import *
-from PolygonTriangulation426274 import *
-from PolygonTriangulation426273 import *
-from PolygonTriangulation426365 import *
-from ReportBuilder426366 import *
-from ReportBuilder426277 import *
-from Rubik426278 import *
-from SharpChess426283 import *
-from SplineInterpolation426284 import *
-from SplineInterpolation426287 import *
-from SplineInterpolation426286 import *
-from SplineInterpolation426285 import *
-from SVGPad426279 import *
-from SVGPad426280 import *
-from SVGPad426281 import *
-from SVGPad426282 import *
-from UsingWebBrowser556344 import *
-from UsingWebBrowser556343 import *
+from winforms.alltests import *
+
 
 if __name__ == '__main__':
     monotesting_main()
